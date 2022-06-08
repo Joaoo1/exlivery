2c686dacc937c034ff522be9c651c46b9f59c7dc
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-06-07 23:16:41 -0300

    tests: orders

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,27 @@ defmodule Exlivery.Factory do
       cpf: "12312312300",
       age: 20,
       address: "Rua teste"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza",
+      category: :pizza,
+      unity_price: Decimal.new("35.5"),
+      quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua teste",
+      items: [
+        build(:item),
+        build(:item, description: "Temaki", category: :japonesa)
+      ],
+      total_price: Decimal.new("71.00"),
+      user_cpf: "12312312300"
     }
   end
 end
