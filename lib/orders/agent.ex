fab860cbc25ba046518db84815d69f1532a9c273
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-06-18 00:10:52 -0300

    feat: list all orders

--- lib/orders/agent.ex
+++ lib/orders/agent.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Orders.Agent do
-  alias Exlivery.Orders.Order
-
   use Agent
+
+  alias Exlivery.Orders.Order
 
   def start_link(_initial_state) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
@@ -16,6 +16,8 @@ defmodule Exlivery.Orders.Agent do
   end
 
   def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
+
+  def list_all, do: Agent.get(__MODULE__, & &1)
 
   defp get_order(state, uuid) do
     case Map.get(state, uuid) do
