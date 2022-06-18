abade72ff9f2ec919b9a6a7fa0c4b18e26fdae61
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-06-18 00:39:23 -0300

    feat: orders csv report

---
+++ lib/orders/report.ex
@@ -0,0 +1,26 @@
+defmodule Exlivery.Orders.Report do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.Order
+  alias Exlivery.Orders.Item
+
+  def create(filename \\ "report.csv") do
+    order_list = build_order_list()
+
+    File.write(filename, order_list)
+  end
+
+  def build_order_list() do
+    OrderAgent.list_all()
+    |> Map.values()
+    |> Enum.map(fn order -> order_string(order) end)
+  end
+
+  defp order_string(%Order{user_cpf: cpf, items: items, total_price: total_price}) do
+    items_string = Enum.map(items, &item_string(&1))
+    "#{cpf},#{items_string},#{total_price}\n"
+  end
+
+  defp item_string(%Item{category: category, quantity: quantity, unity_price: unity_price}) do
+    "#{category},#{quantity},#{unity_price}"
+  end
+end
